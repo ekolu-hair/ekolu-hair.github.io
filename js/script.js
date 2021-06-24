dfeaab6a2dbef19b24b8bb246ef479adf2c8bcd8
R-T-job <3rd103@gmail.com>
2021-06-25 07:33:52 +0900

    clean コメント削除

--- js/script.js
+++ js/script.js
@@ -28,20 +28,10 @@ $(function(){
 		} 		
 	});
 	
-	
-	// $('.panel').hide();
 	$("#menu").click(function() {
 		$(".panel").toggle(function(){
 			$(this).next().slideToggle();
 			$('#menuBtn').toggleClass('close');
 		});
 	});
-	// $('#menuWrap').toggle(function(){
-	// 	$(this).next().slideToggle();
-	// 	$('#menuBtn').toggleClass('close');
-	// },
-	// function(){
-	// 	$(this).next().slideToggle();
-	// 	$('#menuBtn').removeClass('close');
-	// });
 });
